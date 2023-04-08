e27135174039f1a0621f4ec93925073cd3e8d7ec
Gregory <gregory.cobena@gmail.com>
2023-04-08 17:58:49 +0200

    first workling version, not reviewed yet

---
+++ assignment2/assignment2-starter-code/app.js
@@ -0,0 +1,84 @@
+(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyControllerImpl)
+    .controller('AlreadyBoughtController', AlreadyBoughtControllerImpl)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffServiceImpl)
+ //   .config(ConfigToFeedWithInitialData);
+    
+    console.log("starting");
+
+    ToBuyControllerImpl.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyControllerImpl(ShoppingListCheckOffService) {
+
+        this.items = ShoppingListCheckOffService.getBuyList();
+
+        this.alreadyBought = function(itemIndex) {
+        console.log("user wants to declare item already bought :", itemIndex);
+    
+        ShoppingListCheckOffService.declareItemBought(itemIndex);
+        }
+
+        this.isEmpty = function () {
+            return (this.items.length == 0);
+        }
+    }
+
+    AlreadyBoughtControllerImpl.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtControllerImpl(ShoppingListCheckOffService) {
+        this.items = ShoppingListCheckOffService.getAlreadyBoughtList();
+
+        this.isEmpty = function () {
+            return (this.items.length == 0);
+        }
+    }
+
+    function ShoppingListCheckOffServiceImpl() {
+
+        console.log("build service");
+
+        var toBuyList = [];
+        var alreadyBoughtList = [];
+
+        // default initialization
+        toBuyList.push({ name: 'Blue Cookies', quantity : 10});
+        toBuyList.push({ name: 'Green Cookies', quantity : 20});
+        toBuyList.push({ name: 'Yellow Cookies', quantity : 30});
+        toBuyList.push({ name: 'White Cookies', quantity : 40});
+        toBuyList.push({ name: 'Brown Cookies', quantity : 50});
+        toBuyList.push({ name: 'Black Cookies', quantity : 60});
+        toBuyList.push({ name: 'Red Cookies', quantity : 70});
+
+        this.getBuyList = function () {
+            return toBuyList;
+        }
+        this.getAlreadyBoughtList = function () {
+            return alreadyBoughtList;
+        }
+        this.declareItemBought = function (idx) {
+            console.log("mark item as bought : ", idx);
+
+            var item = toBuyList.splice(idx,1);
+            alreadyBoughtList.push(item[0]);
+
+            console.log(alreadyBoughtList);
+        }
+    }
+
+    // ConfigToFeedWithInitialData.$inject = ['ShoppingListCheckOffService'];
+    // function ConfigToFeedWithInitialData(ShoppingListCheckOffService) {
+
+    //     console.log("build config");
+
+    //     var buyList = this.ShoppingListCheckOffService.getBuyList();
+    //     buyList.push({ name: 'Blue Cookies', quantity : 10});
+    //     buyList.push({ name: 'Green Cookies', quantity : 20});
+    //     buyList.push({ name: 'Yellow Cookies', quantity : 30});
+    //     buyList.push({ name: 'White Cookies', quantity : 40});
+    //     buyList.push({ name: 'Brown Cookies', quantity : 50});
+    //     buyList.push({ name: 'Black Cookies', quantity : 60});
+    //     buyList.push({ name: 'Red Cookies', quantity : 70});
+    // }
+
+})();
