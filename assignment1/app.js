befa48b6784cf2792a4c2305790bb898c5715597
Gregory <gregory.cobena@gmail.com>
2023-04-08 16:50:23 +0200

    just various tests, not for the assignment

--- assignment1/app.js
+++ assignment1/app.js
@@ -4,6 +4,35 @@
     angular.module('LunchCheck', [])
     .controller('LunchCheckController', glcControl); 
 
+    var numberArray = [ 5, 4, 3, 1, 7, 8, 4, 3];
+    console.log(numberArray);
+
+    function plusGrandQue(value, reference) {
+        return (value > reference);
+    }
+
+    var filteredArray = numberArray.filter(plusGrandQue, 3);
+    console.log(filteredArray);
+
+    var parent = {
+        speed: 1,
+        method: "toto",
+       getMethod : function() {
+        return this.method;
+       },
+       walk : function () {
+            console.log("parent walking at speed ", this.speed);
+            console.log("with method", this.getMethod());
+        } 
+    };
+
+    var child = Object.create(parent);
+    child.speed = 2;
+    child.getMethod = function() {
+        return "child method: " + this.method;
+    };
+    child.walk(); // il y a bien la notion de methode virtuelle puisque l'on appelle "walk" sur parent, et finallement le "getMethod" du child est appelé
+
     glcControl.$inject = ['$scope'];
     function glcControl($scope) {
 
