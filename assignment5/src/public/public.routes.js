c5e111f2630b9c6dae3f93fa38c98cd885806a61
Gregory <gregory.cobena@gmail.com>
2023-04-20 14:50:33 +0200

    signup form page, without any controller yet, but live validation ok

--- assignment5/src/public/public.routes.js
+++ assignment5/src/public/public.routes.js
@@ -40,6 +40,11 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html',
     });
+
 }
 })();
